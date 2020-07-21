d0d3e2b113dd53c7bc14d55eb9570f80e6ce0edd
ducchienit <ducchienit2k1@gmail.com>
2020-07-21 09:46:22 +0700

    optimize code js

--- js/script.js
+++ js/script.js
@@ -1,109 +1,60 @@
 $("#myInput").on("keyup",function(e){
-
     if(e.keyCode == 13 && $("#myInput").val() != ""){
-    var tr = $("<tr></tr>");
-    var td = $("<td></td>").toggleClass("col-md-12");
-    var label = $("<label></label>");
-    var value = ($("#myInput").val());
-    var checkbox = $("<input/>").toggleClass("abcd").attr("type", "checkbox").on("click", function(event){
-        var listElement = $("#taskTable tbody tr td").length;
-        event.target.classList.toggle('checked');
-        var checkList = $(".checked").length;
-        var percent = checkList/listElement*100;
-        $("#percent").css("width", percent + "%");
-        $("#comp").text(Math.floor(percent) + "%" + " " + "complete");
-    });
-    var close = $("<span>x</span>").toggleClass("close").attr("style", "padding: 0px 5px;").click(function(){
-        var p = $(this).parent();
-        p.fadeOut(function(){
-        p.remove();
-        var listElement = $("#taskTable tbody tr td").length;
-        var checkList = $(".checked").length;
-        var percent = checkList/listElement*100;
-        $("#percent").css("width", percent + "%");
-        $("#comp").text(Math.floor(percent) + "%" + " " + "complete");
-        if(listElement === 0){
-            $("#percent").css("width", 0 + "%");
-            $("#comp").text(Math.floor(0) + "%" + " " + "complete");
-        }
-        });
-    });
-    
-    tr.append(td.append(label.append(checkbox, value),close));
-    $("#elementList").append(tr);  
-    $("#myInput").val("");
-    }
-    var listElement = $("#taskTable tbody tr td").length;
-    var checkList = $(".checked").length;
-    var percent = checkList/listElement*100;
-    $("#percent").css("width", percent + "%");
-    $("#comp").text(Math.floor(percent) + "%" + " " + "complete");
-        
+        let tr = $("<tr></tr>");
+        let td = $("<td></td>").toggleClass("col-md-12");
+        let label = $("<label></label>");
+        let value = ($("#myInput").val());
+        let checkbox = $("<input/>").toggleClass("abcd").attr("type", "checkbox");
+        let close = $("<span>x</span>").toggleClass("close").attr("style", "padding: 0px 5px;");
+        tr.append(td.append(label.append(checkbox, value),close));
+        $("#elementList").append(tr);  
+        $("#myInput").val("");
+        widthProgress();    
+    }  
 });
-var width = 0;
-var percent = 0;
-var load = setInterval(loading, 500);
+$(document).on("click",".abcd", widthProgress); 
+$(document).on("click",".close", function(){
+    $(this).parents('tr').fadeOut().remove();
+    widthProgress();
+});
+function widthProgress(){
+    let listChecked = $('#taskTable input:checked').length;
+    let listTrB1 = $('#taskTable tbody tr').length;
+    let progressB1 = listChecked/listTrB1*100;
+    $("#percent").css("width", progressB1 + "%");
+    $("#comp").text(Math.floor(progressB1) + "% complete");
+    if(listTrB1 === 0){
+        $("#percent").css("width", 0 + "%");
+        $("#comp").text(Math.floor(0) + "% complete");
+    };
+};
+let width = 0;
+let load = setInterval(loading, 500);
 function loading() {
     width += 1;
-    percent += 1;
     $("#percent2").css("width", width + "%");
-    $("#comp2").text(percent + "%" + " " + "complete");
-    if (width == 100 && percent == 100) {
+    $("#comp2").text(width + "% complete");
+    if (width == 100) {
         clearInterval(load);
     }
 }
-$("#check-all-left").on("click", function(){
-    $('#tableleft input').prop('checked', this.checked); 
-})
-$("#check-all-right").on("click", function(){
-    $('#tableright input').prop('checked', this.checked);
-})
-$("#go-to-right").click(function(){
-    $("#tableleft input:checked").not("#check-all-left").each(function(){
-        $(this).prop('checked', false).parents("TR").remove().appendTo("#tableright");
-    })
-})
-$("#go-to-left").click(function(){
-    $("#tableright input:checked").not("#check-all-right").each(function(){
-        $(this).prop('checked', false).parents("TR").remove().appendTo("#tableleft");
-    })
-})
-$("[name='search-left']").on("keyup", function(){
-    var search = $(this).val().toLowerCase();
-    $('#tableleft tbody tr').filter(function(){
-        $(this).toggle($(this).text().toLowerCase().indexOf(search) > -1);
-    })
-})
-$("[name='search-right']").on("keyup", function(){
-    var search = $(this).val().toLowerCase();
-    $('#tableright tbody tr').filter(function(){
-        $(this).toggle($(this).text().toLowerCase().indexOf(search) > -1);
-    })
-})
-$("[name='bai1']").next().hide();
-$("[name='bai2']").next().hide();
-$("[name='bai3']").next().hide();
-$("[name='bai1']").click(function(){
-    $(this).toggleClass('bai1');
-    if($("[name='bai1']").hasClass("bai1")){
-        $("[name='bai1']").next().show();
-    } else{
-        $("[name='bai1']").next().hide();
-    }
-})
-$("[name='bai2']").click(function(){
-    $(this).toggleClass('bai2');
-    if($("[name='bai2']").hasClass("bai2")){
-        $("[name='bai2']").next().show();
-    } else{
-        $("[name='bai2']").next().hide();
-    }
-})
-$("[name='bai3']").click(function(){
-    $(this).toggleClass('bai3');
-    if($("[name='bai3']").hasClass("bai3")){
-        $("[name='bai3']").next().show();
-    } else{
-        $("[name='bai3']").next().hide();
-    }
-})
+function checkAll(){
+    let listCheckbox = $(this).parents('table').children('tbody').find("*").not("tr, td");
+    listCheckbox.prop('checked', this.checked);
+}
+$("#check-all-left, #check-all-right").on("click", checkAll);
+$("#go-to-right, #go-to-left").click(move);
+function move(){
+    let listMove =  $(this).parents('.col-md-6').children('.col-md-10').find('tbody input:checked');
+    let not = $(this).parents('.col-md-6').find('.col-md-10 table');
+    let locationMove = $(this).parents('.panel-body').find('.col-md-6 .col-md-10 table').not(not);
+    listMove.prop("checked", false).parents('tr').remove().appendTo(locationMove);
+}
+function search(){
+    let value = $(this).val().toLowerCase();
+    let listName = $(this).parent().next().children('tbody').find('tr');
+    listName.filter(function(){
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+}
+$("[name='search-left'], [name='search-right']").on("keyup", search);
